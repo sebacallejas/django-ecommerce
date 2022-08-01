431b757cbeb3bae5d7d65e11f1d36a540498e8e6
sebacallejas <sebastian.callejas@gmail.com>
2022-08-01 19:58:31 -0400

    First commit django-ecommerce

---
+++ accounts/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from accounts.models import Account
+
+
+class AccountAdmin(UserAdmin):
+    list_display = ('id', 'username', 'first_name', 'last_name',
+                    'email', 'last_login', 'date_joined', 'is_active')
+    list_display_link = ('email', 'first_name', 'last_name')
+    readonly_fields = ('last_login', 'date_joined')
+    ordering = ('-date_joined',)
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+
+# Register your models here.
+admin.site.register(Account, AccountAdmin)
